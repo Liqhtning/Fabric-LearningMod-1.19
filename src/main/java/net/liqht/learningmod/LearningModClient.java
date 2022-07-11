5b0b8ec1383eb29b67004f50e58d4611dabbcdd2
BuildTools <schoolsmarty@hotmail.com>
2022-07-11 00:24:32 -0500

    Initial commit

---
+++ src/main/java/net/liqht/learningmod/LearningModClient.java
@@ -0,0 +1,10 @@
+package net.liqht.learningmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LearningModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
